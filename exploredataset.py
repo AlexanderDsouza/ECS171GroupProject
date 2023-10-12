e8f8b8d8a285c6dd7c6d13d99c628eb89afb1dec
Alex D'Souza <113999925+AlexanderDsouza@users.noreply.github.com>
2023-10-12 13:52:09 -0700

    Add files via upload

---
+++ exploredataset.py
@@ -0,0 +1,17 @@
+pip install ucimlrepo
+
+
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+mushroom = fetch_ucirepo(id=73) 
+  
+# data (as pandas dataframes) 
+X = mushroom.data.features 
+y = mushroom.data.targets 
+  
+# metadata 
+print(mushroom.metadata) 
+  
+# variable information 
+print(mushroom.variables) 
